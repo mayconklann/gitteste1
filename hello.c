2c47e9a2300376a9012ddcf4f0a6d2d84ee9f085
Maycon Klann <maycon.klann@gmail.com>
2020-09-08 22:51:57 -0300

    Adicionado funcionalidade de soma.

--- hello.c
+++ hello.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
- 
- int main(){
-   printf("Hello World!");
-   return 0;
+  
+  int main(){
+    int ret;
+    ret = soma(2, 3);
+    printf("O resultado é: %d", ret);
+    return 0;
   }
